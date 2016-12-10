a88ce6029fc8960ff56137320670dbec09798721
Hà Minh Tùng <tung.haminh1995@gmail.com>
2016-12-10 21:18:05 +0700

    Sửa lỗi nhỏ khiến giao diện trang chủ bị lệch so với dự kiến

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html lang="en">
 <head>
 
@@ -1059,7 +1059,14 @@
 		</div>
 		
 	</footer><!--/Footer-->
-	
+	<div class="footer-bottom">
+			<div class="container">
+				<div class="row">
+					<p class="pull-left">Copyright © 2013 E-SHOPPER Inc. All rights reserved.</p>
+					<p class="pull-right">Designed by <span><a target="_blank" href="http://www.themeum.com">Themeum</a></span></p>
+				</div>
+			</div>
+		</div>
 
   
     <script src="js/jquery.js"></script>
