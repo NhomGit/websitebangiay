e85482e5cfc03a85d668264fae028107d78b9d56
Hà Minh Tùng <tung.haminh1995@gmail.com>
2016-12-12 01:17:08 +0700

    Bổ xung hàm lọc tên hàng hóa chi tiết hơn

--- chucnangtimkiem.php
+++ chucnangtimkiem.php
@@ -27,6 +27,18 @@
 					</div>	
 				</div>
 			</div>
+<div class="col-sm-4">
+				<div class="product-image-wrapper">
+					<div class="single-products">
+						<div class="productinfo text-center">
+							<img src="<?php echo $value["IMAGE"] ?>" alt="" width="70" height="230" />
+							<h2><?php echo number_format($value["PRICE"]) ?></h2>
+							<p><?php echo $value["PRONAME"] ?></p>
+							<a href="index.php?mod=cart&id=<?php echo $id;?>" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a> <a href="index.php?proname=<?php echo $name;?>&id=<?php echo $id;?>" class="btn btn-default add-to-cart"><i class="fa fa-info"></i>Chi tiết</a>
+						</div>
+					</div>	
+				</div>
+			</div>
 		<?php }
 		}
 	}?>
